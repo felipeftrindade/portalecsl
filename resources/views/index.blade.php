e7470b988dd275de7e9413c8a1a7f0deee586d0f
Felipe Trindade <felipeftrindade@gmail.com>
2018-05-26 18:35:53 -0300

    fixing css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,8 +49,8 @@
   									</div>
   									<h4><a href="{{'/'.$post->url}}">{{$post->title}}</a></h4>
   									<div class="blog-meta">
-  										<span class="col-xs-6">Por: {{$post->Author->name}}</span>
-  										<div class="col-xs-6">
+  										<span class="col-xs-6 nopadding">Por: {{$post->Author->name}}</span>
+  										<div class="col-xs-6 nopadding">
   											<span>{{date('d M Y à\s H:i', strtotime($post->created_at))}}</span>
   											<span class="blog-meta-comments"><a href="{{'/'.$post->url}}#disqus_thread" data-disqus-identifier="{{$post->url}}"></a></span>
   										</div>
